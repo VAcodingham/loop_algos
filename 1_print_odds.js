a620726b110405ac96c84c6678990955e000cd02
Rowan <rowan.smith01@gmail.com>
2020-02-18 13:36:41 -0500

    solutions

--- 1_print_odds.js
+++ 1_print_odds.js
@@ -1,4 +1,9 @@
 //Print out only odd numbers from 1 to 20
 //The expected output will be 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
 
-//YOUR CODE HERE
+var num = 0;
+for(i = 0; i <= 20; i++){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
--- 4_extra_practice.js
+++ 4_extra_practice.js
@@ -2,4 +2,20 @@
 // I.E.  If you wrote both of your algorithms using for loops, now write them out with while loops
 
 
-// YOUR CODE HERE
+var i = 0;
+while(i<=30){
+    if(i%3==0){
+        console.log("Fizz");
+    }
+
+    if(i%5==0){
+        console.log("Buzz");
+    }
+    if(i%3,5==0){
+        console.log("FizzBuzz");
+    }
+    else{
+        console.log(i); 
+}
+i++;
+}
